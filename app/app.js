72417d1cb86dad398ecb2abcd249261528b83550
Jeremy Ho <jujaga@gmail.com>
2021-04-16 15:01:04 -0700

    Add optional configuration to split output logs to file and stdout
    Signed-off-by: Jeremy Ho <jujaga@gmail.com>

--- app/app.js
+++ app/app.js
@@ -1,10 +1,12 @@
 const compression = require('compression');
 const config = require('config');
 const express = require('express');
+const fs = require('fs');
 const log = require('npmlog');
 const morgan = require('morgan');
 const path = require('path');
 const Problem = require('api-problem');
+const Writable = require('stream').Writable;
 
 const apiRoutes = require('./src/cdogsService/routes');
 const keycloak = require('./src/components/keycloak');
@@ -23,13 +25,34 @@ app.use(express.urlencoded({ extended: true }));
 log.level = config.get('server.logLevel');
 log.addLevel('debug', 1500, { fg: 'cyan' });
 
+let logFileStream;
+let teeStream;
+if (config.has('server.logFile')) {
+  // Write to logFile in append mode
+  logFileStream = fs.createWriteStream(config.get('server.logFile'), { flags: 'a' });
+  teeStream = new Writable({
+    objectMode: true,
+    write: (data, _, done) => {
+      process.stdout.write(data);
+      logFileStream.write(data);
+      done();
+    }
+  });
+  log.disableColor();
+  log.stream = teeStream;
+}
+
 // Print out configuration settings in verbose startup
 log.verbose('Config', utils.prettyStringify(config));
 
 // Skip if running tests
 if (process.env.NODE_ENV !== 'test') {
+  const morganOpts = {};
+  if (config.has('server.logFile')) {
+    morganOpts.stream = teeStream;
+  }
   // Add Morgan endpoint logging
-  app.use(morgan(config.get('server.morganFormat')));
+  app.use(morgan(config.get('server.morganFormat'), morganOpts));
 }
 
 // Use Keycloak OIDC Middleware
